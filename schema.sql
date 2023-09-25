cb9cf04e8f85c8028690dac7a3fd15b8d94d8469
Ikonde Nekemiah Arnold <i-nad@localhost.localdomain>
2023-09-25 17:46:59 +0300

    adjusted position of SQL commands

--- schema.sql
+++ schema.sql
@@ -63,13 +63,13 @@ INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT * FROM (SELECT id F
 This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
 insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
 
-CREATE INDEX index_animal_id ON visits (animal_id);
-ANALYZE animals;
+CREATE INDEX idx_animal_id ON visits (animal_id);
+ANALYZE visits;
 EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
 
-CREATE INDEX index_vet_id ON visits (vet_id);
+CREATE INDEX idx_vet_id ON visits (vet_id);
 ANALYZE visits;
 EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
 
-CREATE INDEX index_email ON owners (email);
+CREATE INDEX idx_email ON owners (email);
 EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
